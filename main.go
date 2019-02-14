f5b8f7224470abdf8302a1bd573d18229e04f5e0
cropse <stu0219@hotmail.com>
2019-02-14 14:28:43 +0800

    clean up mess

--- main.go
+++ main.go
@@ -11,38 +11,17 @@ import (
 	"time"
 )
 
-// type flagSet struct {
-// 	*flag.FlagSet
-// }
-
-// func myNewFlagSet(name string, errorHandling flag.ErrorHandling) *myFlagSet {
-// 	f := flag.NewFlagSet(name, errorHandling)
-// 	newf := &myFlagSet{f}
-// 	return newf
-// }
-
-// func (f *myFlagSet) Parsed() bool {
-// 	return f.parsed
-// }
-
-// Fi
 func main() {
 	// Init parser
-	// var erHandling flag.ErrorHandling
-	// myFlag := flag.NewFlagSet("flag", erHandling)
-	// fmt.Println(myFlag.Parsed())
 	flag.Usage = func() {
-		// fmt.Fprintf(os.Stderr, "This is not helpful.\n")
 		fmt.Fprintf(flag.CommandLine.Output(), "Usage of %s:\n", os.Args[0])
 		fmt.Fprintf(flag.CommandLine.Output(), "job-terminator [option] command\n")
 		flag.PrintDefaults()
 	}
 	killer := flag.Bool("killer", false, "The one who Kill process")
-	// flag.Parse(os.Args[1:])
 	flag.Parse()
 	fmt.Printf("killer: %t\n", *killer)
 
-	//
 	subArgs := flag.Args()
 	cmd := exec.Command(subArgs[0], subArgs[1:]...)
 	log.Printf("Running command and waiting hook to terminated")
@@ -51,7 +30,6 @@ func main() {
 	} else {
 		Sender(cmd)
 	}
-
 }
 
 // Sender is the process who going to trigger the webhook to kill it self when it done.
@@ -63,7 +41,6 @@ func Sender(cmd *exec.Cmd) {
 	res, err := http.Get("http://127.0.0.1:8080/kill")
 	if err != nil {
 		log.Fatal(err)
-		os.Exit(2)
 	}
 	defer res.Body.Close()
 	response, err := ioutil.ReadAll(res.Body)
@@ -74,7 +51,6 @@ func Sender(cmd *exec.Cmd) {
 	if string(response) == "OK" {
 		fmt.Println("")
 	}
-
 }
 
 // Receiver is main terminated process running http server to exit process
@@ -88,6 +64,5 @@ func Receiver(cmd *exec.Cmd) {
 		}()
 
 	})
-
 	log.Fatal(http.ListenAndServe("127.0.0.1:8080", nil))
 }
